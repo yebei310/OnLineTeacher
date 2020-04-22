6cb8207d7c87026a1389efcdaf7de9c664ed38ed
yebeibei@pxjy.com <yebeibei@pxjy.com>
2020-04-22 19:44:11 +0800

    fir2

--- src/main/java/testcases/Timetable.java
+++ src/main/java/testcases/Timetable.java
@@ -1,21 +1,24 @@
 package testcases;
 
 
-import net.sf.cglib.asm.$ClassReader;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+
 
 import java.util.Set;
 
 public class Timetable {
 
 
-        WebDriver driver;
+    WebDriver driver;
         @BeforeMethod
         public void setUp () throws Exception {
 
@@ -48,7 +51,12 @@ public class Timetable {
             driver.switchTo().window((String) set.toArray()[1]);
             driver.navigate().refresh();
             Thread.sleep(8000);
-            driver.findElement(By.xpath("//*[@id=\"class-panel-container\"]/div[5]/div")).click();
+            try {
+                driver.findElement(By.xpath("//*[@id=\"class-panel-container\"]/div[5]/div")).click();
+            } catch (Exception e){
+                System.out.printf("已经处于上课中。");
+            }
+
             Thread.sleep(10000);
         }
         @AfterMethod
